01625da071a6770241de503829b8e250fe57a2a2
Junshoong <junshoong@gmail.com>
2017-05-26 00:00:34 +0900

    606점 달성

--- core/integrate.py
+++ core/integrate.py
@@ -3,22 +3,22 @@ from modules.can_access import can_access
 from modules.count_numOfdot import numOfdot
 from modules.has_password_field import has_password_field
 from modules.is_masquerading import is_masquerading
+from modules.html_has_same_domain import html_has_same_domain
+
 
 def integrate(url):
     result = "U"
 
     r = can_access(url)
     if r != "U":
+        r = is_masquerading(url)
+        if r != "U":
+            return r
+        r = html_has_same_domain(url)
+        if r != "U":
+            return r
         r = has_password_field(url)
         if r != "U":
             return r
-    r = url_name_check(url)
-    if r != "U":
-        return r
-    r = is_masquerading(url)
-    if r != "U":
-        return r
-    r = numOfdot(url)
-    if r != "U":
-        return r
+
     return result

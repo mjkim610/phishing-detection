7456abf9c094597294be30982671fd8a501fe3d0
Junshoong <junshoong@gmail.com>
2017-05-25 20:52:56 +0900

    통합

--- core/integrate.py
+++ core/integrate.py
@@ -1,13 +1,21 @@
-from modules.naver import main
+from modules.url_check import *
 from modules.can_access import can_access
-from modules.has_keyword_naver import has_keyword_naver
-from modules.is_naver_domain import is_naver_domain
+from modules.count_numOfdot import numOfdot
 
 
 def integrate(url):
-    result = main(url)
-    # result = can_access(url)
-    print url, result
-    # result = has_keyword_naver(url)
-    # result = is_naver_domain(url)
+    result = "U"
+
+    r = url_name_check(url)
+    if r != "U":
+        return r
+    r = is_naver_domain(url)
+    if r != "U":
+        return r
+    r = numOfdot(url)
+    if r != "U":
+        return r
+    r = can_access(url)
+    if r != "U":
+        return r
     return result
--- core/modules/can_access.py
+++ core/modules/can_access.py
@@ -1,10 +1,16 @@
 import requests
 
+
 def can_access(url):
+    print('can_access')
+    answer = "U"
     try:
-        current_page = (requests.get(url).text, 'lxml')
+        current_page = (requests.get(url, timeout=3).text, 'lxml')
         answer = "SL"
     except requests.exceptions.ConnectionError:
         print("ERROR: Page is inaccessible, return U and move to next case.")
-        answer = "U"
+    except requests.exceptions.Timeout as e:
+        print e
+    except requests.TooManyRedirects as e:
+        print e
     return answer

1d5ebee4ed6b0b7b7a644e9789cb7fabbfc0fae8
mjkim610 <mjkim610@gmail.com>
2017-05-25 18:06:08 +0900

    Add is_naver_domain feature and can_access feature

--- mjkim610/scrap.py
+++ mjkim610/scrap.py
@@ -12,6 +12,10 @@ from datetime import datetime
 #             CONSTANTS
 # -----------------------------------
 """
+URL = 'http://220.230.122.163/'
+START = 'api/start'
+STOP = 'api/stop'
+KEY = 'user_cat'
 
 """
 # -----------------------------------
@@ -19,6 +23,8 @@ from datetime import datetime
 # -----------------------------------
 """
 def main():
+    #res = requests.post(URL + START, json={'key': KEY, 'type': "N"})
+    #print(res.text)
     phishing_urls = read_list()
     get_features(phishing_urls)
 
@@ -34,26 +40,54 @@ def get_features(phishing_urls):
 
     with open('results.txt', 'w') as writeFile:
         # Write header
-        writeFile.write("id,is_naver\n") # need to make file spliced by some other character? or will comma be okay
+        writeFile.write("id,can_access,has_keyword_naver,is_naver_domain\n")
 
         id = 0
-        is_naver = 0
+        can_access = 0
+        has_keyword_naver = 0
+        is_naver_domain = 0
 
         for url in phishing_urls:
             url = url.rstrip()
             print("Analyzing " + url + "...")
+
+            # Check can_access
             try:
                 current_page = (requests.get(url).text, 'lxml')
-
-                if "naver.com" in url:
-                    is_naver = 1
-                else:
-                    is_naver = 0
+                can_access = 1
             except requests.exceptions.ConnectionError:
                 print("Connection error. Check URL.")
-                is_naver = -1
+                can_access = 0
 
-            writeFile.write(str(id) + "," + str(is_naver) + "\n")
+            # Check has_keyword_naver.com
+            if "naver.com" in url:
+                has_keyword_naver = 1
+            else:
+                has_keyword_naver = 0
+
+            # Check is_naver_domain
+            temp = url.index("/")
+            try:
+                temp = url[temp+2:].index("/") + temp+2
+            except:
+                temp = len(url) + temp+2
+
+            url_parsed = url[:temp]
+            domain_end = temp
+
+            temp = url_parsed.rfind('.')
+            domain_start = url_parsed[:temp].rfind('.')
+            if domain_start == -1:
+                domain_start = url_parsed[:temp].rfind('/')
+            domain = url[domain_start+1:domain_end]
+            print(domain)
+
+            if domain == "naver.com":
+                is_naver_domain = 1
+            else:
+                is_naver_domain = 0
+
+            writeFile.write(str(id) + "," + str(can_access) + "," + str(has_keyword_naver) + "," + str(is_naver_domain) + "\n")
             id += 1
             #sleep(1)
     print("Finished analysis!")

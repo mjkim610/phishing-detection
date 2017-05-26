c349bb69fc4b87e7b5414c78ba3e612a1def6697
Junshoong <junshoong@gmail.com>
2017-05-26 11:28:41 +0900

    get_root_domain 함수 분리 및 일부 개선

--- core/modules/html_has_same_domain.py
+++ core/modules/html_has_same_domain.py
@@ -1,15 +1,5 @@
 from bs4 import BeautifulSoup as bs
-
-
-def get_root_domain(url):
-    try:
-        l = url.split('/')
-        if l[0] in ['http:', 'https:']:
-            return ".".join(l[2].split('.')[-2:])
-    except AttributeError as e:
-        print(e)
-
-    return False
+from get_root_domain import get_root_domain
 
 
 def html_has_same_domain(url, resp):
@@ -19,9 +9,10 @@ def html_has_same_domain(url, resp):
 
     current_page = bs(resp.text, 'lxml')
     for tag in current_page.find_all('a'):
-        in_url = get_root_domain(tag.get('href'))
-        if in_url == root:
-            cnt += 1
+        if tag.get('href'):
+            in_url = get_root_domain(tag.get('href'))
+            if in_url == root:
+                cnt += 1
 
     if cnt >= 1:
         return "S", mod
